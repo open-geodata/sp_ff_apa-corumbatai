cb31b782522516e46cd9c548afb8b314e4638b27
Michel Metran <michelmetran@gmail.com>
2022-06-04 21:20:04 -0300

    Ajustes

--- test/paths.py
+++ test/paths.py
@@ -1,18 +1,21 @@
 import os
 
-data_path = os.path.join('..', 'data')
-bruto_path = os.path.join(data_path, 'bruto')
-input_path = os.path.join(data_path, 'input')
-output_path = os.path.join(data_path, 'output')
+data_path = os.path.join('..', 'src', 'data')
 
+bruto_path = os.path.join(data_path, 'bruto')
 os.makedirs(bruto_path, exist_ok=True)
+
+input_path = os.path.join(data_path, 'input')
 os.makedirs(input_path, exist_ok=True)
+
+output_path = os.path.join(data_path, 'output')
 os.makedirs(output_path, exist_ok=True)
 
 output_path_maps = os.path.join(output_path, 'maps')
-output_path_gpkg = os.path.join(output_path, 'gpkg')
-output_path_zips = os.path.join(output_path, 'zips')
-
 os.makedirs(output_path_maps, exist_ok=True)
+
+output_path_gpkg = os.path.join(output_path, 'gpkg')
 os.makedirs(output_path_gpkg, exist_ok=True)
+
+output_path_zips = os.path.join(output_path, 'zips')
 os.makedirs(output_path_zips, exist_ok=True)
