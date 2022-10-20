80418b34c605b1200ef8c722378f6630f8e5da5f
Michel Metran <michelmetran@gmail.com>
2022-10-20 00:46:28 -0300

    Ajustes

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,24 +1,61 @@
-import os
+"""
 
-data_path = os.path.join('..', 'src', 'sp_ff_apa_corumbatai', 'data')
 
-input_path = os.path.join(data_path, 'input')
-os.makedirs(input_path, exist_ok=True)
+"""
 
-bruto_path = os.path.join(input_path, 'bruto')
-os.makedirs(bruto_path, exist_ok=True)
 
-extract_path = os.path.join(input_path, 'extract')
-os.makedirs(extract_path, exist_ok=True)
+from pathlib import Path
 
-output_path = os.path.join(data_path, 'output')
-os.makedirs(output_path, exist_ok=True)
 
-output_path_maps = os.path.join(output_path, 'maps')
-os.makedirs(output_path_maps, exist_ok=True)
+# Project Path
+project_path = Path(__file__).parents[1]
 
-output_path_gpkg = os.path.join(output_path, 'gpkg')
-os.makedirs(output_path_gpkg, exist_ok=True)
+# scrapy_path = project_path / 'scrapy'
+# scrapy_path.mkdir(exist_ok=True)
 
-output_path_zips = os.path.join(output_path, 'zips')
-os.makedirs(output_path_zips, exist_ok=True)
+# logs_path = scrapy_path / 'logs'
+# logs_path.mkdir(exist_ok=True)
+
+# adds_path = scrapy_path / 'adds'
+# adds_path.mkdir(exist_ok=True)
+
+
+# Package Path
+package_path = project_path / 'sp_piracicaba'
+
+data_path = package_path / 'data'
+data_path.mkdir(exist_ok=True)
+
+input_path = data_path / 'input'
+input_path.mkdir(exist_ok=True)
+
+input_path_bruto = input_path / 'bruto'
+input_path_bruto.mkdir(exist_ok=True)
+
+input_path_extract = input_path / 'extract'
+input_path_extract.mkdir(exist_ok=True)
+
+output_path = data_path / 'output'
+output_path.mkdir(exist_ok=True)
+
+output_path_geojson = output_path / 'geojson'
+output_path_geojson.mkdir(exist_ok=True)
+
+output_path_gpkg = output_path / 'gpkg'
+output_path_gpkg.mkdir(exist_ok=True)
+
+# output_path_tab = output_path / 'tab'
+# output_path_tab.mkdir(exist_ok=True)
+
+output_path_map = output_path / 'map'
+output_path_map.mkdir(exist_ok=True)
+
+output_path_shp = output_path / 'shp'
+output_path_shp.mkdir(exist_ok=True)
+
+output_path_zip = output_path / 'zips'
+output_path_zip.mkdir(exist_ok=True)
+
+
+if __name__ == '__main__':
+    print(project_path)
